b884da17e8432c8f59e521786b5041c9f735d2ed
Dr. Dvorak <171316552+DocDvorak@users.noreply.github.com>
2026-08-09 06:56:40 -0700

    chore: Rewrite reset-docker.sh to remove curl service requirement

--- reset-docker.sh
+++ reset-docker.sh
@@ -1,14 +1,25 @@
-docker compose down -v
-docker compose up -d
-until docker compose run curl -X POST http://localhost:21028/cli --remove-orphans -d 'system.resetAllData()'; do
-  echo "Waiting for server..."
-  sleep 10
-done
-docker compose restart screeps
-until docker compose run curl -X POST http://localhost:21028/cli --remove-orphans -d 'utils.addNPCTerminals()'; do
-  echo "Waiting for server..."
-  sleep 5
-done
-docker compose run curl -X POST http://localhost:21028/cli --remove-orphans -d 'storage.db["rooms.objects"].insert({ room: "W0N5", type: "portal", x: 25, y: 25, destination: { x: 25, y: 25, room: "W10N5" } });'
-docker compose run curl -X POST http://localhost:21028/cli --remove-orphans -d 'storage.db["rooms.objects"].insert({ room: "W10N5", type: "portal", x: 25, y: 25, destination: { x: 25, y: 25, room: "W0N5" } });'
+#!/usr/bin/env bash
+set -euo pipefail
 
+# Tear down the test environment, recreate it, and wait for all services to be ready
+echo '(Re)creating test environment'
+docker compose down -v
+docker compose up -d --wait
+
+# Pause the server to prevent server from entering an inconsistent state
+echo 'Pausing server'
+docker compose exec screeps cli -c 'system.pauseSimulation()'
+
+# Seed terminals and portals
+echo 'Seeding room objects'
+docker compose exec screeps cli -c 'utils.addNPCTerminals()'
+docker compose exec screeps cli -c 'storage.db["rooms.objects"].insert([{ room: "W0N5", type: "portal", x: 25, y: 25, destination: { x: 25, y: 25, room: "W10N5" } }, { room: "W10N5", type: "portal", x: 25, y: 25, destination: { x: 25, y: 25, room: "W0N5" } }])'
+
+# Restart the server to ensure changes are applied.
+# Wait a moment for the CLI to become available after the server starts.
+echo 'Restarting and resuming server'
+docker compose restart --no-deps screeps
+sleep 5
+docker compose exec screeps cli -c 'system.resumeSimulation()'
+
+echo 'Done'
